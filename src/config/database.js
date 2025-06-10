1add696ef23cbe8361b963921957c04e09100c68
namrata <namrata.jain@pensionbox.in>
2025-06-10 10:19:56 +0530

    UPDATE: connect to the techtribe db instead of whole cluster

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://namratajain29001:WqJKA2CY5eknAAMn@njnode.ygkmvx4.mongodb.net/"
+    "mongodb+srv://namratajain29001:WqJKA2CY5eknAAMn@njnode.ygkmvx4.mongodb.net/TechTribe"
   );
 };
 
