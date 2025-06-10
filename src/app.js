d5c4496e8dc6046f083940ae886a15044b1fcabb
namrata <namrata.jain@pensionbox.in>
2025-06-10 10:16:23 +0530

    FEAT: connect application to the mongodb atlas i.e. database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const { connectDb } = require("./config/database");
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+// connect to the db first and then listen on the port
+connectDb()
+  .then(() => {
+    console.log("Database connection established...")
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  })
+  .catch((err) => console.error("Database cannot be connected!!!"));
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  await mongoose.connect(
+    "mongodb+srv://namratajain29001:WqJKA2CY5eknAAMn@njnode.ygkmvx4.mongodb.net/"
+  );
+};
+
+module.exports = { connectDb };
