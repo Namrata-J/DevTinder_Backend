d7b49d55904896da45f3e5c5ea24ab83108c36a7
namrata <namrata.jain@pensionbox.in>
2025-06-11 10:35:46 +0530

    FEAT: add indexes on connectionRequest Schema

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -18,6 +18,8 @@ const connectionRequestSchema = new Schema({
   },
 });
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
 
