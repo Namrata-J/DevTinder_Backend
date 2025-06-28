87bebe8690332436f2de0f6a96c749b9680c9c22
namrata <namrata.jain@pensionbox.in>
2025-06-28 16:12:06 +0530

    add ec2 instance public address in CORS origin

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'http://13.201.6.106']
 }));
 
 // routers
